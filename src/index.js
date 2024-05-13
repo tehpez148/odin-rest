f53710859ad4074c1d6c414c6549dd3d390daf79
fatman148 <108585064+tehpez148@users.noreply.github.com>
2024-05-13 14:58:04 +0100

    Created function to empty the DOM to replace all elements as needed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import './style.css';
 
 import {homeFunction} from "./home";
+//function to empty the DOM before replacing it with new select elements
+function empty(element) {
+    while(element.firstElementChild) {
+       element.firstElementChild.remove();
+    }
+};
 
+const mainBox = document.getElementById('content');  
+  
 const home = document.getElementById('home');
 
 const menu = document.getElementById('menu');
 
 const about = document.getElementById('about')
 
-home.addEventListener('click', () => {homeFunction()});
+home.addEventListener('click', () => {
+    empty(mainBox);
+    homeFunction();
+});
 
 
 menu.addEventListener('click', () => alert("menu is happening"));
