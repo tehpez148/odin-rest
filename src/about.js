43ecc4f6bef1772b561f3dd4d1604caa294360f4
fatman148 <108585064+tehpez148@users.noreply.github.com>
2024-05-14 14:18:31 +0100

    Completed website with filler content and basic styling

--- src/about.js
+++ src/about.js
@@ -14,12 +14,20 @@ const aboutFunction = () => {
     const aboutSecond = document.createElement('h2');
     aboutSecond.textContent = "Interested in a summary of our porky endevours?"
 
+    const firstPara = document.createElement('h4');
+    const secondPara = document.createElement('h4');
+
+    firstPara.textContent = "We've been slinging sausages since Sixty-Six. Our cylindrical dream began when our founder, the great silly sausage himself, grew tired of the dull, lifeless and limp meat holders that dare call themselves sausages in this town. Since then, we've strived to succeed at sausage science."
+    secondPara.textContent = "We seriously select only the most superior pig farms to source a select sausage insides. We take time to really get in there with the pigs and much and such to ensure we're delivering the best quality SNACKS. We then take that porcine goodness and lovingly pump them into tenderly selected skins. We then deep fry them in the most re-used oil this side of the atlantic, to give that genuine silly sausage taste."
 
 
     topBox.appendChild(aboutHead)
     topBox.appendChild(aboutSecond);
     mainBox.appendChild(topBox);
 
+    botBox.appendChild(firstPara);
+    botBox.appendChild(secondPara);
+    mainBox.appendChild(botBox);
 
 };
 
--- src/menu.js
+++ src/menu.js
@@ -11,15 +11,44 @@ const menuFunction = () => {
     const menuSecond = document.createElement('h2');
     menuSecond.textContent = "Browse our bountiful brace of bouncy sausages";
     
+   
 
 
     topBox.appendChild(menuHead);
     topBox.appendChild(menuSecond);
     mainBox.appendChild(topBox);
 
+    const menuSign = document.createElement('h2');
+    menuSign.textContent= "Menu";
+
+    const menuItems = document.createElement('div');
+    menuItems.setAttribute('id','menuitems');
+
+    const item1 = document.createElement('h3');
+    item1.textContent = "Lorem Sausage_____£4";
+    const item2 = document.createElement('h3');
+    item2.textContent = "Ipsum Sausage_____£4";
+    const item3 = document.createElement('h3');
+    item3.textContent = "Cheese Sausage_____£4";
+    const item4 = document.createElement('h3');
+    item4.textContent = "Chilly Sausage_____£3";
+
+    menuItems.appendChild(item1);
+    menuItems.appendChild(item2);
+    menuItems.appendChild(item3);
+    menuItems.appendChild(item4);
+   
+   
 
 
 
+
+
+
+
+    botBox.appendChild(menuSign);
+    botBox.appendChild(menuItems);
+    mainBox.appendChild(botBox);
 
 
 };
