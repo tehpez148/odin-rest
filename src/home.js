e5d6b7613e3c7be8e97eb02313cb35f7c2fb4439
fatman148 <108585064+tehpez148@users.noreply.github.com>
2024-05-14 11:36:29 +0100

    added transition styles to Buttons, added content to main page

--- src/home.js
+++ src/home.js
@@ -9,23 +9,32 @@ const homeFunction = () => {
     const botBox = document.createElement('div');
     botBox.classList.add('bottom');
 
+    const mySosig = new Image();
+    mySosig.src = Sosig;
 
-    //const innerBox = document.createElement('div');
 
     const welcomeHead = document.createElement('h1');
     welcomeHead.textContent= "Welcome to Silly Sausage Sanctuary";
     const welcomeSecond = document.createElement('h2');
     welcomeSecond.textContent = "Are you in the mood for some funky, spunky sausages?"
 
-    
+    const firstPara = document.createElement('h4');
+    const secondPara = document.createElement('h4');
+
+    firstPara.textContent = "Welcome to our special sausage zone. Where all your most salubrious sausage-y desires can be answered. Anything sausage shaped, we've got for you. So take a look around!"
+    secondPara.textContent = "Fancy a spicy salami? A sweet slim-jim? A seductive saucisson? We've got whatever tickles your pickle (we've even got pickles!)"
     topBox.appendChild(welcomeHead);
     topBox.appendChild(welcomeSecond);
     mainBox.appendChild(topBox);
 
+    botBox.appendChild(firstPara);
+    botBox.appendChild(mySosig);
+    botBox.appendChild(secondPara);
+    mainBox.appendChild(botBox);
 
-    const mySosig = new Image();
-    mySosig.src = Sosig;
-    mainBox.appendChild(mySosig);
+
+    
+    
 
 
 };
